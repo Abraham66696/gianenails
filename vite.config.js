e8375f18df55acfe7b100bcfaa7fced49acc1a37
Carim <tituloabraham@gmail.com>
2026-07-26 17:44:30 -0300

    Fix: Configurar Vite para usar web.html como entry point

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: './web.html'
+    }
+  },
   server: {
     port: 5173,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
   }
 })
